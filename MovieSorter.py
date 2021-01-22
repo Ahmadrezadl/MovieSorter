37afc54818615888e9db90a18402ac1053ca220b
Ahmadreza <46781518+Ahmadrezadl@users.noreply.github.com>
2021-01-22 03:30:28 +0330

    add format name
    better coding

--- MovieSorter.py
+++ MovieSorter.py
@@ -1,63 +1,13 @@
 import os
-import locale
-from tkinter import messagebox, Menu, Entry
-from tkinter.filedialog import askdirectory, re, Frame, Checkbutton, Button, LEFT, RIGHT, TOP, BOTTOM, W, E, Tk, \
-    StringVar, Label, IntVar, YES, X, sys
-from easysettings import EasySettings
+import sys
+import re
+from tkinter import *
+from tkinter import messagebox,filedialog
 
-settings = EasySettings("settings.conf")
-if not settings.has_option('Language'):
-    if locale.getdefaultlocale() == "fa_IR":
-        settings.set("Language", "Persian")
-    else:
-        settings.set("Language", "English")
-    settings.save()
-if settings.get('Language') == 'Persian':
-    nameOfProgram = "Movie Sorter"
-    chooseFileName = "انتخاب فایل"
-    quitText = 'خروج'
-    startText = 'شروع دسته بندی'
-    folderForEpisodes = 'فولدر سازی برای قسمت ها'
-    putYearInBrackets = 'قرار دادن سال ساخت داخل پرانتز'
-    moviesText = 'فیلم ها'
-    seriesText = 'سریال ها'
-    infoText = 'درباره ما'
-    settingsText = ':تنظیمات'
-    formatsText = ':فرمت ها'
-    getAncher = 'e'
-    first = 'right'
-    last = 'left'
-    chooseDirText = 'انتخاب فولدر مورد نظر'
-    completeMessage = "دسته بندی انجام شد"
-    nameChange = "هم اسم کردن فیلم و زیرنویس"
-    changeLanguage = "English"
-    exitText = "خروج"
-    openText = "باز کردن"
+from load_language import *
 
-else:
-    nameOfProgram = "Movie Sorter"
-    chooseFileName = "Choose Directory"
-    quitText = 'Quit'
-    startText = 'Start Grouping'
-    folderForEpisodes = 'Folder for Episodes'
-    putYearInBrackets = 'Put Year of Release Date in Brackets '
-    moviesText = 'Movies'
-    seriesText = 'Series'
-    infoText = 'About Us'
-    settingsText = ' Settings:'
-    formatsText = ' Formats:'
-    getAncher = 'w'
-    first = 'left'
-    last = 'right'
-    chooseDirText = 'Choose Folder Directory'
-    completeMessage = "Completed!"
-    nameChange = "Same Name for Subtitle and Movie"
-    changeLanguage = "فارسی"
-    exitText = "Exit"
-    openText = "Open"
 
 def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
         base_path = sys._MEIPASS
     except Exception:
@@ -65,7 +15,6 @@ def resource_path(relative_path):
 
     return os.path.join(base_path, relative_path)
 
-###############################################
 
 class CheckBar(Frame):
     def __init__(self, parent=None, picks=[], side=LEFT, anchor=getAncher, defaultcheck=False):
@@ -87,29 +36,35 @@ if __name__ == '__main__':
     root = Tk()
     root.resizable(False, False)
 
+
     def changeLanguageEvent():
         if getAncher == 'e':
-            settings.set('Language' , 'English')
+            settings.set('Language', 'English')
             settings.save()
-            answer = messagebox.askyesno( "Language Changed!", "For Apply changes you need to reopen the program.\nRestart now?")
+            answer = messagebox.askyesno("Language Changed!",
+                                         "For Apply changes you need to reopen the program.\nRestart now?")
             if answer:
-                os.execl(sys.executable,*([sys.executable]+sys.argv))
+                os.execl(sys.executable, *([sys.executable] + sys.argv))
 
         else:
             settings.set('Language', 'Persian')
             settings.save()
-            answer = messagebox.askyesno("زبان برنامه تغییر کرد" , "برای اعمال تغییرات باید برنامه از اول اجرا شود\nبارگذاری مجدد برنامه؟")
+            answer = messagebox.askyesno("زبان برنامه تغییر کرد",
+                                         "برای اعمال تغییرات باید برنامه از اول اجرا شود\nبارگذاری مجدد برنامه؟")
             if answer:
-                os.execl(sys.executable,*([sys.executable]+sys.argv))
+                os.execl(sys.executable, *([sys.executable] + sys.argv))
+
 
     def about():
         ctypes.windll.user32.MessageBoxW(0, "An App By: Ahmad Reza Kamali & Mahdi Sabour", infoText, 0)
+
+
     def chooseDir():
         Tk().withdraw()
         options = {}
         options['title'] = chooseFileName
         global chooseDirString
-        chooseDirString = askdirectory(**options)
+        chooseDirString = filedialog.askdirectory(**options)
         if len(chooseDirString) < 57:
             chooseDirVar.set(chooseDirString)
         else:
@@ -133,17 +88,19 @@ if __name__ == '__main__':
             numberOfFilesVar.set("شروع دسته بندی" + str(numberOfFiles) + ' فایل')
         else:
             numberOfFilesVar.set('Start Grouping ' + str(numberOfFiles) + " Files")
+
+
     menubar = Menu(root)
     subMenu = Menu(menubar, tearoff=0)
     menubar.add_cascade(label="File", menu=subMenu)
-    subMenu.add_command(label=openText , command=chooseDir)
-    subMenu.add_command(label=exitText , command=root.quit)
+    subMenu.add_command(label=openText, command=chooseDir)
+    subMenu.add_command(label=exitText, command=root.quit)
     root.config(menu=menubar)
 
     subMenu = Menu(menubar, tearoff=0)
     menubar.add_cascade(label="Help", menu=subMenu)
-    subMenu.add_command(label=infoText,command=about)
-    subMenu.add_command(label=changeLanguage,command=changeLanguageEvent)
+    subMenu.add_command(label=infoText, command=about)
+    subMenu.add_command(label=changeLanguage, command=changeLanguageEvent)
     root.iconbitmap(resource_path('Logo.ico'))
     var = StringVar()
     Label(root, textvariable=var, anchor=getAncher).pack(side=TOP, fill='both')
@@ -151,8 +108,10 @@ if __name__ == '__main__':
     willRemove = CheckBar(root, [folderForEpisodes, putYearInBrackets])
     willRename = CheckBar(root, [nameChange], defaultcheck=True)
 
-    videoFiles = CheckBar(root, [moviesText, seriesText , '.mkv' , '.mp4' , '.avi' , '.srt','.zip'],defaultcheck=True)
-    modelCreator = Entry(root)
+    videoFiles = CheckBar(root, [moviesText, seriesText, '.mkv', '.mp4', '.avi', '.srt', '.zip'], defaultcheck=True)
+    model = StringVar()
+    model.set("{name} - {year}")
+    modelCreator = Entry(root,textvariable= model,font="Calibri 15")
     modelCreator.pack(side=TOP, fill=X)
     willRemove.pack(side=TOP, fill=X)
     willRename.pack(side=TOP, fill=X)
@@ -169,15 +128,19 @@ if __name__ == '__main__':
     chooseDirString = ""
     numberOfFiles = 0
     numberOfFilesVar = StringVar()
+    def openAll():
+        for root, dirs, files in os.walk(chooseDirString):
+            for file in files:
+                print(os.path.join(root, file))
+                os.rename(os.path.join(root, file), chooseDirString + "\\" + file)
+            if not os.listdir(root):
+                os.rmdir(root)
+
 
     if getAncher == 'e':
         numberOfFilesVar.set('شروع دسته بندی')
     else:
         numberOfFilesVar.set('Start Grouping')
-    # videoFiles.config(relief=GROOVE, bd=1)
-    # willRemove.config(relief=GROOVE, bd=2)
-
-
 
 
 
@@ -185,56 +148,51 @@ if __name__ == '__main__':
         list(videoFiles.state())[3]
         for file in os.listdir(chooseDirString):
             if file.lower().endswith(".mkv") and list(videoFiles.state())[2] == 1:
-                folderName = file.replace(".mkv", "")
+                folder_name = file.replace(".mkv", "")
             elif file.lower().endswith(".mp4") and list(videoFiles.state())[3] == 1:
-                folderName = file.replace(".mp4", "")
+                folder_name = file.replace(".mp4", "")
             elif file.lower().endswith(".avi") and list(videoFiles.state())[4] == 1:
-                folderName = file.replace(".avi", "")
+                folder_name = file.replace(".avi", "")
             elif file.lower().endswith(".srt") and list(videoFiles.state())[5] == 1:
-                folderName = file.replace(".srt", "")
+                folder_name = file.replace(".srt", "")
             elif file.lower().endswith(".zip") and list(videoFiles.state())[6] == 1:
-                folderName = file.replace(".zip", "")
-            # elif file.lower().endswith(".zip"):
-            #     with zipfile.ZipFile(chooseDirString + "\\" + file, 'r') as zip_ref:
-            #         zip_ref.extractall(chooseDirString)
+                folder_name = file.replace(".zip", "")
             else:
                 continue
-            folderName = folderName.replace(".", " ")
-            folderName = folderName.replace("_", " ")
-            folderName = folderName.replace("-", " ")
-            folderName = folderName.replace("(" , "")
-            folderName = folderName.replace(")" , "")
-            folderName = folderName.replace("  ", " ")
-            folderName = folderName.replace("  ", " ")
+            folder_name = folder_name.replace(".", " ")
+            folder_name = folder_name.replace("_", " ")
+            folder_name = folder_name.replace("-", " ")
+            folder_name = folder_name.replace("(", "")
+            folder_name = folder_name.replace(")", "")
+            folder_name = folder_name.replace("  ", " ")
+            folder_name = folder_name.replace("  ", " ")
 
-            folderWords = re.split(r'\s', folderName)
+            folderWords = re.split(r'\s', folder_name)
             # Serial or Movie? Let's Find out
-            folderName = ""
+            folder_name = ""
             serial = False
             season = '01'
             episode = '01'
             firstS = True
             for s in folderWords:
-                if(firstS):
+                if (firstS):
                     firstS = False
-                    folderName += str(s) + " "
-                    print(folderName)
+                    folder_name += str(s) + " "
                     continue
                 if len(s) == 6:
                     if (s[0] == 'S' or s[0] == 's') and (s[3] == 'E' or s[3] == 'e') and (s[1].isdigit()):
                         serial = True
                         season = 'S' + str(s[1]) + str(s[2])
                         episode = 'E' + str(s[4]) + str(s[5])
-                        if folderName.endswith(" "):
-                            folderName = folderName[:-1]
+                        if folder_name.endswith(" "):
+                            folder_name = folder_name[:-1]
                         break
-                folderName += str(s) + " "
-                print(folderName)
+                folder_name += str(s) + " "
 
             #################################
             if serial and list(videoFiles.state())[1] == 1:
                 newDir = chooseDirString
-                newDir = newDir + "\\" + folderName
+                newDir = newDir + "\\" + folder_name
                 try:
                     os.mkdir(newDir)
                 except OSError:
@@ -257,48 +215,34 @@ if __name__ == '__main__':
                     else:
                         print("Fucking Nice")
                 os.rename(os.path.join(chooseDirString, file), newDir + "\\" + file)
-            print((not serial) and list(videoFiles.state())[0] == 1)
             if (not serial) and list(videoFiles.state())[0] == 1:
-                folderName = ""
-                print(folderWords)
+                folder_name = ""
                 firstS = True
                 year = 0000
                 name = ""
                 for s in folderWords:
-                    if (firstS):
-                        firstS = False
-                        folderName += str(s) + " "
-                        name += str(s)
-                        continue
-                    if s.isdigit():
-                        if int(s) > 1900 and int(s) < 3000:
+                    if s.isdigit() and not firstS:
+                        if 1900 < int(s) < 3000:
                             year = int(s)
-                            if list(willRemove.state())[1] == 1:
-                                folderName += "(" + s + ")"
-                                ...
-                            else:
-                                folderName += s
                             break
-                        else:
-                            name += s + " "
-                            folderName += s + " "
                     else:
-                        folderName += s + " "
-
-                if folderName.endswith(" "):
-                    folderName = folderName[:-1]
-                if modelCreator.get() != '':
-                    folderName = str(modelCreator.get())
-                    folderName = folderName.replace('&year',str(year))
-                    folderName = folderName.replace('&name', str(name))
+                        firstS = False
+                        name += str(s) + ' '
+                        continue
+                if name.endswith(' '):
+                    name = name[0:-1]
+                folder_name = str(modelCreator.get())
+                if list(willRemove.state())[1]:
+                    folder_name = folder_name.replace('{year}', "(" + str(year) + ")")
+                else:
+                    folder_name = folder_name.replace('{year}', str(year))
+                folder_name = folder_name.replace('{name}', str(name))
                 newDir = chooseDirString
-                newDir = newDir + "\\" + folderName
+                newDir = newDir + "\\" + folder_name
                 try:
                     os.mkdir(newDir)
                 except OSError:
-                    print("Fucking Error")
-                else:
-                    print("Fucking Nice")
+                    print(OSError)
 
                 os.rename(os.path.join(chooseDirString, file), newDir + "\\" + file)
 
@@ -311,7 +255,7 @@ if __name__ == '__main__':
                 mkvName = ""
                 for file in os.listdir(f):
                     if file.lower().endswith(".mkv"):
-                        mkvName = file.replace(".mkv",".srt")
+                        mkvName = file.replace(".mkv", ".srt")
                         break
                     else:
                         continue
@@ -320,30 +264,25 @@ if __name__ == '__main__':
                         try:
                             os.rename(os.path.join(f, file), f + "\\" + mkvName)
                         except FileExistsError:
-                            print('Fucking Fuck')
+                            print('Multiple subtitle')
                         break
                     else:
                         continue
         import ctypes
         ctypes.windll.user32.MessageBoxW(0, completeMessage, infoText, 0)
 
-    import ctypes
-    def openAll(link):
-        for root, dirs, files in os.walk(link):
-            for file in files:
-                print(os.path.join(root, file))
-                os.rename(os.path.join(root, file), link + "\\" + file)
-            if not os.listdir(root):
-                os.rmdir(root)
 
-    # openAll("E:\\1-Movies\\! Other Movies")
+    import ctypes
+
+
     btn = Button(root, textvariable=chooseDirVar, command=chooseDir).pack(side=TOP, fill='both')
+    Button(root , text=unfolderString, command=openAll).pack(side=TOP,fill=X)
     chooseDirVar.set(chooseDirText)
-    Frame(root, width=10, height=1,bd=0).pack(side=LEFT)
-    Frame(root, width=10, height=1,bd=0).pack(side=RIGHT)
-    Frame(root, width=5, height=5,bd=0).pack(side=TOP, fill=X)
+    Frame(root, width=10, height=1, bd=0).pack(side=LEFT)
+    Frame(root, width=10, height=1, bd=0).pack(side=RIGHT)
+    Frame(root, width=5, height=5, bd=0).pack(side=TOP, fill=X)
     Button(root, text=quitText, command=root.quit).pack(side=last)
     Button(root, textvariable=numberOfFilesVar, command=createFolder).pack(side=first)
-    Frame(root, width=1, height=35,bd=0).pack(side=BOTTOM, fill=X)
+    Frame(root, width=1, height=35, bd=0).pack(side=BOTTOM, fill=X)
     root.title(nameOfProgram)
     root.mainloop()
---
+++ load_language.py
@@ -0,0 +1,55 @@
+import locale
+from easysettings import EasySettings
+
+settings = EasySettings("settings.conf")
+if not settings.has_option('Language'):
+    if locale.getdefaultlocale() == "fa_IR":
+        settings.set("Language", "Persian")
+    else:
+        settings.set("Language", "English")
+    settings.save()
+if settings.get('Language') == 'Persian':
+    nameOfProgram = "Movie Sorter"
+    chooseFileName = "انتخاب فایل"
+    quitText = 'خروج'
+    startText = 'شروع دسته بندی'
+    folderForEpisodes = 'فولدر سازی برای قسمت ها'
+    putYearInBrackets = 'قرار دادن سال ساخت داخل پرانتز'
+    moviesText = 'فیلم ها'
+    seriesText = 'سریال ها'
+    unfolderString = 'درووردن فایل ها از فولدر ها'
+    infoText = 'درباره ما'
+    settingsText = ':فرمت اسم فایل'
+    formatsText = ':فرمت ها'
+    getAncher = 'e'
+    first = 'right'
+    last = 'left'
+    chooseDirText = 'انتخاب فولدر مورد نظر'
+    completeMessage = "دسته بندی انجام شد"
+    nameChange = "هم اسم کردن فیلم و زیرنویس"
+    changeLanguage = "English"
+    exitText = "خروج"
+    openText = "باز کردن"
+
+else:
+    nameOfProgram = "Movie Sorter"
+    chooseFileName = "Choose Directory"
+    quitText = 'Quit'
+    startText = 'Start Grouping'
+    folderForEpisodes = 'Folder for Episodes'
+    putYearInBrackets = 'Put Year of Release Date in Brackets '
+    moviesText = 'Movies'
+    seriesText = 'Series'
+    infoText = 'About Us'
+    settingsText = ' Name of file format:'
+    formatsText = ' Formats:'
+    getAncher = 'w'
+    first = 'left'
+    last = 'right'
+    unfolderString='Unfolder all folders'
+    chooseDirText = 'Choose Folder Directory'
+    completeMessage = "Completed!"
+    nameChange = "Same Name for Subtitle and Movie"
+    changeLanguage = "فارسی"
+    exitText = "Exit"
+    openText = "Open"
